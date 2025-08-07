570459dfe0fbf5d5c2103bdd89d4448abd1524e5
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-07 15:27:52 -0700

    .

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,19 +1,45 @@
 @extends('web::layouts.app')
 
-@section('page_title', 'Home')
-
+@section('page-title', 'Welcome')
+@section('page-icon', 'fas fa-home')
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            @forelse ($homeElements as $element)
+
+<div class="container-fluid">
+    {{-- Row 1 - 6 widgets @ col-md-2 --}}
+    <div class="row mb-3">
+        @foreach($homeElements->where('row', 1) as $element)
+            <div class="col-md-2 mb-3">
                 {!! $element['html'] !!}
-            @empty
-                <div class="col-12">
-                    <div class="alert alert-info">
-                        No home elements have been registered yet.
-                    </div>
-                </div>
-            @endforelse
-        </div>
+            </div>
+        @endforeach
     </div>
+
+    {{-- Row 2 - 4 widgets @ col-md-3 --}}
+    <div class="row mb-3">
+        @foreach($homeElements->where('row', 2) as $element)
+            <div class="col-md-3 mb-3">
+                {!! $element['html'] !!}
+            </div>
+        @endforeach
+    </div>
+
+    {{-- Row 3 - 2 widgets @ col-md-6 --}}
+    <div class="row mb-3">
+        @foreach($homeElements->where('row', 3) as $element)
+            <div class="col-md-6 mb-3">
+                {!! $element['html'] !!}
+            </div>
+        @endforeach
+    </div>
+
+    {{-- Row 4 - 1 widget @ col-md-12 --}}
+    <div class="row mb-3">
+        @foreach($homeElements->where('row', 4) as $element)
+            <div class="col-md-12 mb-3">
+                {!! $element['html'] !!}
+            </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
