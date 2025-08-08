8a7d71ee0728204109dd9793dd4e697f3a3a08c3
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-08 11:52:19 -0700

    .

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -138,7 +138,7 @@
                     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
                     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-                    document.getElementById('dd-time').textContent =
+                    document.getElementById('dt-time').textContent =
                         `T minus ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
                 }
 
