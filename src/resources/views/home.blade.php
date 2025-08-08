b49d967c8154ef898875a714665b3cafce35a092
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-08 11:41:58 -0700

    .

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -74,7 +74,7 @@
 <div class="home-wrap">
 
   {{-- ALERT BANNER --}}
-    @if($atWar)
+    @if($atWar === false)
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <i class="fas fa-crosshairs mr-2"></i>
         You are currently At War!
