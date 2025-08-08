d9162ab77152362586fc6a20e294ae0c180c9ca9
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-08 11:36:55 -0700

    .

--- src/Http/Controllers/HomeOverrideController.php
+++ src/Http/Controllers/HomeOverrideController.php
@@ -16,26 +16,26 @@ class HomeOverrideController extends Controller
         $atWar = false;
 
         // Loop through each character
-        foreach ($user->characters as $char) {
+        // foreach ($user->characters as $char) {
 
 
-            $esi = new Eseye();
+        //     $esi = new Eseye();
 
-            $info = $esi->invoke('get', '/characters/{character_id}/', [
-                'character_id' => $char->character_id,
-            ]);
+        //     $info = $esi->invoke('get', '/characters/{character_id}/', [
+        //         'character_id' => $char->character_id,
+        //     ]);
 
-            // Get an authenticated Eseye client from SeAT
-            $esi = $char->getEsi();
+        //     // Get an authenticated Eseye client from SeAT
+        //     $esi = $char->getEsi();
 
-            $corpId     = $info->corporation_id;
-            $allianceId = property_exists($info, 'alliance_id') ? $info->alliance_id : null;
+        //     $corpId     = $info->corporation_id;
+        //     $allianceId = property_exists($info, 'alliance_id') ? $info->alliance_id : null;
 
-            if ($corpId && $this->isAtWar($esi, $corpId, $allianceId)) {
-                $atWar = true;
-                break;
-            }
-        }
+        //     if ($corpId && $this->isAtWar($esi, $corpId, $allianceId)) {
+        //         $atWar = true;
+        //         break;
+        //     }
+        // }
 
 
         $homeElements = collect(config('osmm.home_elements', []))->sortBy('order');
