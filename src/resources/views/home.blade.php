494ef30cdb1278b3251c9945ca3847f7810ffdae
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-13 12:54:16 -0700

    .

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -4,9 +4,7 @@
 @section('page_title', 'Home')
 
 @section('content')
-<script>
-    console.log(@json($publicInfo));
-</script>
+
   <div class="container-fluid">
     <div class="row">
     {{-- MAIN --}}
