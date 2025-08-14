f7c471147e2a08d7aed80bb230b9a331555654b7
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-14 10:32:06 -0700

    .

--- src/config/sidebar.php
+++ src/config/sidebar.php
@@ -4,7 +4,7 @@ return [
     'osmm' => [
         'name'       => 'OSMM Config',
         'icon'       => 'fa fa-cogs',
-        'route_segment'      => 'seat-osmm.config.branding', // must match your route name
+        'route_segment'      => 'osmm.config.branding', // must match your route name
         'permission' => 'osmm.admin',
         // no 'entries' => [...] means this item itself is clickable
         'order'      => 95, // optional: position relative to other top-level items
@@ -12,7 +12,7 @@ return [
             [
                 'name' => 'Config Manager',
                 'icon' => 'fa fa-cogs',
-                'route' => 'seat-osmm.config.branding',
+                'route' => 'osmm.config.branding',
                 'permission' => 'osmm.admin',
             ],
         ],
