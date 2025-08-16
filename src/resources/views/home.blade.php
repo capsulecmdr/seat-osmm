0e60c856533d0f85fa83b5386e58a94dc17e1f28
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-16 13:06:03 -0700

    .

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -34,6 +34,15 @@
   </style>
   <div class="container-fluid">
     <div class="row">
+      <!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Bootstrap
+  </a>
+</nav>
+    </div>
+    <div class="row">
       
     {{-- MAIN --}}
     <div class="col-xl-8">
