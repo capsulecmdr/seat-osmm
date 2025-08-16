f3851db6e99b9413dd50438d3b0c0b8ccaa7da2b
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-16 16:28:19 -0700

    .

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -36,7 +36,7 @@
     <div class="row">
       <!-- Image and text -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <a class="navbar-brand" href="/home"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -51,8 +51,11 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Pricing</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
     </ul>
-    <span class="navbar-text">
+    <span id="cc-time">
       Navbar text with an inline element
     </span>
   </div>
