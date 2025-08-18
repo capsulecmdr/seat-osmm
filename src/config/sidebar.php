3a9e60cfe254d89c8f612966a49c28f7dfee021b
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-18 14:57:11 -0700

    .

--- src/config/sidebar.php
+++ src/config/sidebar.php
@@ -1,20 +1,5 @@
 <?php
 
 return [
-    'osmm' => [
-        'name'       => 'OSMM Config',
-        'icon'       => 'fa fa-cogs',
-        'route_segment'      => 'osmm.config.branding', // must match your route name
-        'permission' => 'osmm.admin',
-        // no 'entries' => [...] means this item itself is clickable
-        'order'      => 95, // optional: position relative to other top-level items
-        'entries' => [
-            [
-                'name' => 'Branding',
-                'icon' => 'fa fa-map-signs',
-                'route' => 'osmm.config.branding',
-                'permission' => 'osmm.admin',
-            ],
-        ],
-    ],
+    
 ];
