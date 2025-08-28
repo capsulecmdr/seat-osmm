2df2d811a4c468bdc664b36617dc73c684b1696e
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-28 13:34:50 -0700

    .

--- src/resources/views/maintenance/landing.blade.php
+++ src/resources/views/maintenance/landing.blade.php
@@ -161,6 +161,19 @@
     </div>
     <script src="{{ asset('vendor/capsulecmdr/seat-osmm/js/metro.js') }}" defer></script>
     <script>
+        var app = new Model({
+            counter: 0,
+            status() {
+                return this.counter === 0
+                    ? "Zero"
+                    : this.counter > 0
+                    ? "Positive"
+                    : "Negative";
+            }
+        });
+
+        app.init("#root");
+        
         const countdown = Metro.getPlugin('#myCountdown', 'countdown');
         countdown.resetWith({
             days: 0,
