e49fa1adc0fed341323c0afedc89d15df758181d
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-28 09:27:55 -0700

    .

--- src/Http/Middleware/OsmmMaintenanceMiddleware.php
+++ src/Http/Middleware/OsmmMaintenanceMiddleware.php
@@ -22,7 +22,7 @@ class OsmmMaintenanceMiddleware
 
         // Allow the maintenance landing itself, auth, and static assets
         if ($request->routeIs('osmm.maint.landing') ||
-            $request->is('login*','logout*','assets*','vendor*','web/*','images/*')) {
+            $request->is('login*','logout*','assets*','vendor*','web/css','web/js','web/ing','images/*')) {
             return $next($request);
         }
 
