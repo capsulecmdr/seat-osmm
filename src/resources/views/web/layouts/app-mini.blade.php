07557888b662d045d900b60958033f8d11cf472f
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-08-30 12:11:31 -0700

    .

--- src/resources/views/web/layouts/app-mini.blade.php
+++ src/resources/views/web/layouts/app-mini.blade.php
@@ -245,9 +245,10 @@
             @endphp
             <button class="outline no-border reduce-2" disabled>{!! $label !!}</button>
             <button class="square outline no-border" disabled style="color:#EEE; background-color:#ff000099"><span class="mif-network"></span></button>
+        @else
+        <button class="square outline no-border"><span class="mif-network"></span></button>
         @endif
-        {{-- @endif
-        <button class="outline no-border reduce-2">ENG</button>
+        {{-- <button class="outline no-border reduce-2">ENG</button>
         <button class="square outline no-border"><span class="mif-network"></span></button>
         <button class="square outline no-border"><span class="mif-keyboard"></span></button>
         <button class="square outline no-border"><span class="mif-power"></span></button> --}}
