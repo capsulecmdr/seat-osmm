c5c286776a03fbd09f301739dafe0dcaa3bbc1e5
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-07 13:15:59 -0700

    .

--- src/config/notifications.alerts.php
+++ src/config/notifications.alerts.php
@@ -1,11 +1,19 @@
 <?php
+// return [
+//     'osmm.maintenance_toggled' => [
+//         'label' => 'osmm::notifications.maintenance_toggled',
+//         'handlers' => [
+//             'mail'    => \CapsuleCmdr\SeatOsmm\Notifications\Mail\MaintenanceToggled::class,
+//             'slack'   => \CapsuleCmdr\SeatOsmm\Notifications\Slack\MaintenanceToggled::class,
+//             'discord' => \CapsuleCmdr\SeatOsmm\Notifications\Discord\MaintenanceToggled::class,
+//         ],
+//     ],
+// ];
 
 return [
     'osmm.maintenance_toggled' => [
         'label' => 'osmm::notifications.maintenance_toggled',
         'handlers' => [
-            'mail'    => \CapsuleCmdr\SeatOsmm\Notifications\Mail\MaintenanceToggled::class,
-            'slack'   => \CapsuleCmdr\SeatOsmm\Notifications\Slack\MaintenanceToggled::class,
             'discord' => \CapsuleCmdr\SeatOsmm\Notifications\Discord\MaintenanceToggled::class,
         ],
     ],
