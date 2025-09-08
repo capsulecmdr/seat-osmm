9831a5ad2b872caf2b1cd4476a0a2c6ed0c3619e
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-08 11:25:01 -0700

    .

--- src/resources/views/includes/announcement-banner.blade.php
+++ src/resources/views/includes/announcement-banner.blade.php
@@ -1,17 +1,19 @@
 @php
-  $banner = \CapsuleCmdr\SeatOsmm\Models\OsmmAnnouncement::active()->get();
+  $banners = \CapsuleCmdr\SeatOsmm\Models\OsmmAnnouncement::active()->get();
 @endphp
 
 @if ($banner->isNotEmpty())
-  <div class="alert alert-warning mb-0 rounded-0 dont-autohide" role="alert" style="position:sticky; top:0; z-index: 1050;">
-    <div class="container d-flex justify-content-between align-items-center">
-      <div class="mr-3">
-        <strong>{{ $banner->title }}</strong>
-        <span class="ml-2">{!! $banner->content !!}</span>
+  @foreach ($banners as $banner)
+    <div class="alert alert-warning mb-0 rounded-0 dont-autohide" role="alert" style="position:sticky; top:0; z-index: 1050;">
+      <div class="container d-flex justify-content-between align-items-center">
+        <div class="mr-3">
+          <strong>{{ $banner->title }}</strong>
+          <span class="ml-2">{!! $banner->content !!}</span>
+        </div>
+        @can('osmm.maint_manage')
+          <a class="btn btn-sm btn-outline-dark" href="{{ route('osmm.maint.config') }}">Manage</a>
+        @endcan
       </div>
-      @can('osmm.maint_manage')
-        <a class="btn btn-sm btn-outline-dark" href="{{ route('osmm.maint.config') }}">Manage</a>
-      @endcan
     </div>
-  </div>
+  @endforeach
 @endif
