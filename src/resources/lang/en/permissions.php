98c46513cd2ed42069082a8316ab2279b1ed7e6e
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-08 14:16:11 -0700

    .

--- src/resources/lang/en/permissions.php
+++ src/resources/lang/en/permissions.php
@@ -3,4 +3,8 @@
 return [
     'permission_osmm_admin_label' => 'Administrator',
     'permission_osmm_admin_desc' => 'Grants full access to all administrative and configuration tools.',
+    'permission_osmm_maint_manage_label' => 'Maintenance Manager',
+    'permission_osmm_maint_manage_desc' => 'Grants access to maintenance management tools.',
+    'permission_osmm_maint_bypass_label' => 'Maintenance Bypass',
+    'permission_osmm_maint_bypass_desc' => 'Allows user to bypass the maintenance landing page.',
 ];
